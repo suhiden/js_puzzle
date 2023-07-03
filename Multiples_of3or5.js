80e53b37cd20072923ad9ca5cc3ad3df4b8e1e29
suhiden <121883200+suhiden@users.noreply.github.com>
2023-07-03 19:45:43 +0500

    Add files via upload
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
    
    Note: If the number is a multiple of both 3 and 5, only count it once.

---
+++ Multiples_of3or5.js
@@ -0,0 +1,22 @@
+function multiplayNumber3or5 (number) {
+    if (number < 0) {
+        return 0;
+    }
+    let result = 0;
+    try {
+        for (let index = 0; index < number; index++){
+                if ((index % 3 == 0) || (index % 5 == 0)) 
+                {
+                result = result + index;
+                console.log(index, "  =   ", result)
+                }          
+            }
+        } 
+    catch (error) {
+        console.log('возникла ошибка = ', error)
+    }
+    
+    return result;
+}
+console.log(multiplayNumber3or5(49494949));
+
