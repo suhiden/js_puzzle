8fe0094e4f99496e23fd2b3e3cfe5e48c5a17e68
suhiden <121883200+suhiden@users.noreply.github.com>
2023-07-04 08:40:28 +0500

    002

--- 002_DescendingOrder/002_DescendingOrder.js
+++ 002_DescendingOrder/002_DescendingOrder.js
@@ -2,7 +2,7 @@ function descendingOrder(number)
 {
   const numberArray = [...''+number].map(Number)
   numberArray.sort((a, b) => b - a);
-  return numberArray.join('')
+  return parseInt(numberArray.join(''))
 }
 
-console.log(descendingOrder(3434524))
+console.log(descendingOrder(938475234))
