9938fc3a8e11944548422087c0ef2c0bfc9cb9c4
suhiden <121883200+suhiden@users.noreply.github.com>
2023-07-04 09:24:09 +0500

    Update 003_SumofNumbers.js

--- 003_SumofNumbers/003_SumofNumbers.js
+++ 003_SumofNumbers/003_SumofNumbers.js
@@ -1,16 +1,18 @@
 function getSum(a, b)
-{
-    let interval = a - b;
-    if (interval === 0 ) {
-        return a;
-    }
+{   
+    let interval = 0;
+    let beginNumber = 0;
+    if (a>b){interval = a - b; beginNumber = b;}         
+    if (a<b){interval = b - a; beginNumber = a;}   
+    if (a == b) {return a;}
+
+    console.log("interval = ", interval)
     let result = 0; 
     for (let index = 0; index <= interval; index++) {
+        result += beginNumber + index;
         console.log("result = ", result, "  index = ", index)
-        result += index 
-        
     }
     return result;
 }
 
-console.log(getSum(3,5))
+console.log(getSum(2,2))
