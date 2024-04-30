20e5c14cd60f4681b92b34c160318dfff504c977
queteriter <queteriter1982@protonmail.com>
2024-04-30 20:30:45 +0300

    easy commit and push

--- 025_/025.ts
+++ 025_/025.ts
@@ -1,3 +1,19 @@
-//NEW puzzle
-//Простая правка и отправка в гитхаб
 
+function islands(array:string[][]){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            let item = array[i][j]
+            console.log(item);
+        }
+
+    }
+    return(array);
+}
+
+let matrix = [["1","1","0","1","1"],
+    ["1","1","0","1","1"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"],
+    ["0","1","1","0","0"]];
+
+console.log(islands(matrix));
